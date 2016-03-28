f1e458f8c72c976fdaeb559414d7f4e57eb8b472
ying <wyingtian@gmail.com>
2016-03-28 15:35:06 -0400

    small name change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,18 +9,18 @@
 
         <div class="col-md-4 text-thin">
             <img class="img-responsive center" src="images/lorem-ipsum.jpg" alt="lorem-ipsum generator image">
-            <a href="{{ action('LoremIpsumController@getRandomText') }}">lorem-Ipsum generator</a>
+            <a href="{{ action('LoremIpsumController@getRandomText') }}">lorem Ipsum (Random text) Generator</a>
 
 
         </div>
         <div class="col-md-4 text-thin">
             <img class="img-responsive center" src="images/user-gen.png" alt="final-project image">
-            <a href="{{ action('UserGeneratorController@getRandomUser') }}">User generator</a>
+            <a href="{{ action('UserGeneratorController@getRandomUser') }}">Random User Generator</a>
 
         </div>
         <div class="col-md-4 text-thin">
             <img class="img-responsive center" src="images/pass-gen.png" alt="password generator image">
-            <a href="{{ action('PasswordGeneratorController@getRandomPassword') }}">Password generator</a>
+            <a href="{{ action('PasswordGeneratorController@getRandomPassword') }}">Random assword Generator</a>
         </div>
     </div>
 
